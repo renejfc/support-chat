d1c9a76606cc0acce0ad3e47b82144c0d5e6501e
ReneJFC <renejfc@gmail.com>
2023-01-29 18:16:47 +0100

    core: init

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+html, body , #__nuxt{
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/pages/chat.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Chat</div>
+</template>
