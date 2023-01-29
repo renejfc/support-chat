76896e3429986c64b9dd9361def14b48591bf1a2
ReneJFC <renejfc@gmail.com>
2023-01-29 20:25:36 +0100

    style: setup workflow & tokens

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src/',
+  css: [
+    '~/styles/tokens/_colors.css',
+    '~/styles/tokens/_typography.css',
+    '~/styles/_global.css',
+  ],
   modules: [
     '@pinia/nuxt',
   ],
--- src/app.vue
+++ src/app.vue
@@ -5,11 +5,3 @@
     </NuxtLayout>
   </div>
 </template>
-
-<style>
-html, body , #__nuxt{
-  height: 100vh;
-  margin: 0;
-  padding: 0;
-}
-</style>
